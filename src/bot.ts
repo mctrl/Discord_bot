f11048996191ea755af3c10f90f3db34cb9d3e17
mctrl <mctrl@users.noreply.github.com>
2019-05-27 12:03:24 +0100

    implement messageResponse class in bot

--- src/bot.ts
+++ src/bot.ts
@@ -6,16 +6,29 @@ import {MessageResponder} from "./services/message-responder";
 export class Bot {
     private client: Client;
     private readonly token: string;
+    private messageResponder: MessageResponder;
     constructor(
         @inject(TYPES.Client) client: Client,
-        @inject(TYPES.Token) token: string
+        @inject(TYPES.Token) token: string,
+        @inject(TYPES.MessageResponder) messageResponder: MessageResponder
     ){
         this.client = client;
         this.token = token;
+        this.messageResponder = messageResponder;
     }
     public listen(): Promise<string> {
         this.client.on('message', (message: Message) => {
+            if (message.author.bot) {
+                console.log('Ignoring bot message!');
+                return;
+            }
             console.log(`message received! content ${message.content}`)
+
+            this.messageResponder.handle(message).then(() => {
+                console.log("Response sent!");
+            }).catch(() => {
+                console.log("Response not sent.")
+            })
         })
         return this.client.login(this.token)
     }
